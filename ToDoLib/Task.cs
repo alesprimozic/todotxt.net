236ad52b97eb29a10bc00c9b68d708e3ee93d6c1
Ales Primozic <ales@primozic.net>
2012-08-28 07:56:34 +0200

    Resolved conflict.

--- ToDoLib/Task.cs
+++ ToDoLib/Task.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.Globalization;
-using System.Linq;
-using System.Text;
-=======
->>>>>>> origin/dev
 using System.Text.RegularExpressions;
 
 namespace ToDoLib
